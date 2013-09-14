9f7ec772b73133e77eac8144e754c6c8a9aefc7d
Ben <benb13@users.noreply.github.com>
2013-09-14 13:48:24 -0400

    Update benrrd.py

--- benrrd.py
+++ benrrd.py
@@ -4,7 +4,7 @@
 # Author:   	Benjamin Bordonaro
 # Copyright:    copyright 2012-2013   
 # Source:       http://www.benjaminbordonaro.com
-# Version:  	13.07.22
+# Version:  	13.07.02
 # Features:
 # - Graphing of Arduino Monitoring System RRDTool DB
 #######################################################
